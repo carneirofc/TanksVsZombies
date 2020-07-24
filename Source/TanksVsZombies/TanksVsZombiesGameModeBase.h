db94909f98d45197d24554b284d269a8be5f5d80
carneirofc <claudiofcarneiro@hotmail.com>
2020-07-24 00:25:39 -0300

    Initial commit

---
+++ Source/TanksVsZombies/TanksVsZombiesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TanksVsZombiesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKSVSZOMBIES_API ATanksVsZombiesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
